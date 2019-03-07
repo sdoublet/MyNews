b3a92f8cdceb1a9e4a8e998bcee3bcfa3f129aa3
sdoublet <doubletsebastien@sfr.fr>
2019-03-07 14:03:18 +0100

    configur PageAdapter

--- app/src/main/java/com/example/doubl/mynews/Controller/Fragments/MostPopularFragment.java
+++ app/src/main/java/com/example/doubl/mynews/Controller/Fragments/MostPopularFragment.java
@@ -12,10 +12,10 @@ import com.example.doubl.mynews.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class TopStories extends Fragment {
+public class MostPopularFragment extends Fragment {
 
 
-    public TopStories() {
+    public MostPopularFragment() {
         // Required empty public constructor
     }
 
@@ -24,7 +24,7 @@ public class TopStories extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_top_stories, container, false);
+        return inflater.inflate(R.layout.fragment_most_pouplar, container, false);
     }
 
 }
--- app/src/main/java/com/example/doubl/mynews/Controller/Views/ViewPager/PageAdapter.java
+++ app/src/main/java/com/example/doubl/mynews/Controller/Views/ViewPager/PageAdapter.java
@@ -2,9 +2,51 @@ package com.example.doubl.mynews.Controller.Views.ViewPager;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 
-public class PageAdapter extends android.support.v4.view.ViewPager {
-    public PageAdapter(@NonNull Context context) {
-        super(context);
+import com.example.doubl.mynews.Controller.Fragments.MostPopularFragment;
+import com.example.doubl.mynews.Controller.Fragments.TechnologyFragment;
+import com.example.doubl.mynews.Controller.Fragments.TopStoriesFragment;
+
+public class PageAdapter extends FragmentPagerAdapter {
+
+
+    public PageAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new MostPopularFragment();
+            case 1:
+                return new TopStoriesFragment();
+            case 2:
+                return new TechnologyFragment();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position){
+            case 0:
+                return "Most Popular";
+            case 1:
+                return "Top Stories";
+            case 2:
+                return "Technology";
+        }
     }
 }
