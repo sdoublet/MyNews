bdd9591f1eb0db23c278165a1200e2cec484596f
sdoublet <doubletsebastien@sfr.fr>
2019-03-07 13:56:10 +0100

    Rename ViewPager class in PageAdapter and create new packages

---
+++ app/src/main/java/com/example/doubl/mynews/Controller/Views/ViewPager/PageAdapter.java
@@ -0,0 +1,10 @@
+package com.example.doubl.mynews.Controller.Views.ViewPager;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+public class PageAdapter extends android.support.v4.view.ViewPager {
+    public PageAdapter(@NonNull Context context) {
+        super(context);
+    }
+}
