3575d1214ee003e0664491762b5470b738caf290
sdoublet <doubletsebastien@sfr.fr>
2019-03-07 21:44:20 +0100

    Configure MostPopularFragment

--- app/src/main/java/com/example/doubl/mynews/Controller/Fragments/MostPopularFragment.java
+++ app/src/main/java/com/example/doubl/mynews/Controller/Fragments/MostPopularFragment.java
@@ -7,12 +7,15 @@ import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.bumptech.glide.Glide;
+import com.example.doubl.mynews.Controller.Models.MostPopular;
 import com.example.doubl.mynews.Controller.Models.ResultMostPopular;
+import com.example.doubl.mynews.Controller.Utils.NewYorkTimesStream;
 import com.example.doubl.mynews.Controller.Views.RecyclerViews.MostPopularAdapter;
 import com.example.doubl.mynews.R;
 
@@ -22,6 +25,7 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableObserver;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -54,17 +58,62 @@ public class MostPopularFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view;
-        view= inflater.inflate(R.layout.fragment_most_pouplar, container, false);
+        view = inflater.inflate(R.layout.fragment_most_pouplar, container, false);
         ButterKnife.bind(view);
         this.configureRecyclerView();
+        this.executeHttpRequestWithRetrofit();
+        this.configureSwipeRefreshLayout();
         return view;
     }
 
-    private void configureRecyclerView(){
+    private void configureRecyclerView() {
         resultMostPopularList = new ArrayList<>();
-        adapter = new MostPopularAdapter(resultMostPopularList, getContext(),Glide.with(this));
+        adapter = new MostPopularAdapter(resultMostPopularList, getContext(), Glide.with(this));
         recyclerView.setAdapter(this.adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
     }
+
+    //------------------
+    // UPDATE UI
+    //------------------
+    public void updateUI(List<ResultMostPopular> article) {
+        swipeRefreshLayout.setRefreshing(false);
+        resultMostPopularList.clear();
+        resultMostPopularList.addAll(article);
+        adapter.notifyDataSetChanged();
+    }
+
+    //----------------------------
+    //HTTP REQUEST WITH RETROFIT
+    //----------------------------
+
+    private void executeHttpRequestWithRetrofit() {
+        this.disposable = NewYorkTimesStream.streamFetchMostPopular(1).subscribeWith(new DisposableObserver<MostPopular>() {
+            @Override
+            public void onNext(MostPopular mostPopular) {
+                updateUI(mostPopular.getResults());
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e("TAG", "On Error...");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e("TAG", "On complete!");
+            }
+        });
+    }
+
+    private void configureSwipeRefreshLayout(){
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                executeHttpRequestWithRetrofit();
+            }
+        });
+    }
+
 
 }
