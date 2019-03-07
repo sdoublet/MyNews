36c2aaa326bbd73a5151fda811b1e85ac5678ebf
sdoublet <doubletsebastien@sfr.fr>
2019-03-07 18:26:53 +0100

    Create streamFetchMostPopular and observable

--- app/src/main/java/com/example/doubl/mynews/Controller/Utils/NewYorkTimesStream.java
+++ app/src/main/java/com/example/doubl/mynews/Controller/Utils/NewYorkTimesStream.java
@@ -1,4 +1,19 @@
 package com.example.doubl.mynews.Controller.Utils;
 
+import com.example.doubl.mynews.Controller.Models.MostPopular;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
 public class NewYorkTimesStream {
+    public static Observable<MostPopular> streamFetchMostPopular (int period){
+        NewYorkTimesServices newYorkTimesServices = NewYorkTimesServices.retrofitMostPopular.create(NewYorkTimesServices.class);
+        return newYorkTimesServices.getNYTMostPopular(period)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .timeout(20,TimeUnit.SECONDS);
+    }
 }
