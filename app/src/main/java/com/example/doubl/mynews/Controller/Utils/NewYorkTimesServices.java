3ba83bf2e5ca901387986954b85ae9d65628ea8d
sdoublet <doubletsebastien@sfr.fr>
2019-03-07 18:23:33 +0100

    configure interface NYTServices with retrofitMostPopular

--- app/src/main/java/com/example/doubl/mynews/Controller/Utils/NewYorkTimesServices.java
+++ app/src/main/java/com/example/doubl/mynews/Controller/Utils/NewYorkTimesServices.java
@@ -1,4 +1,21 @@
 package com.example.doubl.mynews.Controller.Utils;
 
+import com.example.doubl.mynews.Controller.Models.MostPopular;
+
+import io.reactivex.Observable;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface NewYorkTimesServices {
+    @GET("{period}.json?api-key"+ApiKey.API_KEY)
+    Observable<MostPopular>getNYTMostPopular(@Path("period")int period);
+
+    Retrofit retrofitMostPopular = new Retrofit.Builder()
+            .baseUrl("https://api.nytimes.com/svc/mostpopular/v2/viewed/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build();
 }
