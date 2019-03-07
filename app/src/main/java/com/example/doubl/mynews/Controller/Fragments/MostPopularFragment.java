e2991e9f3fdb6d146c545f54eb9739f853507ade
sdoublet <doubletsebastien@sfr.fr>
2019-03-07 18:59:18 +0100

    add glide dependencies and configure MostPopular recyclerview

--- app/src/main/java/com/example/doubl/mynews/Controller/Fragments/MostPopularFragment.java
+++ app/src/main/java/com/example/doubl/mynews/Controller/Fragments/MostPopularFragment.java
@@ -2,12 +2,26 @@ package com.example.doubl.mynews.Controller.Fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.Glide;
+import com.example.doubl.mynews.Controller.Models.ResultMostPopular;
+import com.example.doubl.mynews.Controller.Views.RecyclerViews.MostPopularAdapter;
 import com.example.doubl.mynews.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.reactivex.disposables.Disposable;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,16 +29,42 @@ import com.example.doubl.mynews.R;
 public class MostPopularFragment extends Fragment {
 
 
+    //----------------
+    // FOR DESIGN
+    //----------------
+    @BindView(R.id.recycler_view_most_popular_fragment)
+    RecyclerView recyclerView;
+    @BindView(R.id.swipe_refresh_most_popular)
+    SwipeRefreshLayout swipeRefreshLayout;
+
+    //----------------
+    // FOR DATA
+    //----------------
+
+    private Disposable disposable;
+    private List<ResultMostPopular> resultMostPopularList;
+    private MostPopularAdapter adapter;
+
     public MostPopularFragment() {
         // Required empty public constructor
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_most_pouplar, container, false);
+        View view;
+        view= inflater.inflate(R.layout.fragment_most_pouplar, container, false);
+        ButterKnife.bind(view);
+        this.configureRecyclerView();
+        return view;
+    }
+
+    private void configureRecyclerView(){
+        resultMostPopularList = new ArrayList<>();
+        adapter = new MostPopularAdapter(resultMostPopularList, getContext(),Glide.with(this));
+        recyclerView.setAdapter(this.adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
     }
 
 }
--- app/src/main/java/com/example/doubl/mynews/Controller/Views/RecyclerViews/MostPopularAdapter.java
+++ app/src/main/java/com/example/doubl/mynews/Controller/Views/RecyclerViews/MostPopularAdapter.java
@@ -1,10 +1,27 @@
 package com.example.doubl.mynews.Controller.Views.RecyclerViews;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.RequestManager;
+import com.example.doubl.mynews.Controller.Models.ResultMostPopular;
+
+import java.util.List;
+
 public class MostPopularAdapter extends RecyclerView.Adapter<RecyclerviewHolder> {
+
+    private List<ResultMostPopular> resultMostPopularList;
+    private Context context;
+    private RequestManager glide;
+
+    public MostPopularAdapter(List<ResultMostPopular> resultMostPopularList, Context context, RequestManager glide) {
+        this.resultMostPopularList = resultMostPopularList;
+        this.context = context;
+        this.glide = glide;
+    }
+
     @NonNull
     @Override
     public RecyclerviewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
