878b7e1b5cccc82495301fdbba98d2c835e02f8b
sdoublet <doubletsebastien@sfr.fr>
2019-03-07 14:07:17 +0100

    add Recyclerview library in gradle

--- app/src/main/java/com/example/doubl/mynews/Controller/Views/ViewPager/PageAdapter.java
+++ app/src/main/java/com/example/doubl/mynews/Controller/Views/ViewPager/PageAdapter.java
@@ -1,7 +1,6 @@
 package com.example.doubl.mynews.Controller.Views.ViewPager;
 
-import android.content.Context;
-import android.support.annotation.NonNull;
+
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -40,13 +39,15 @@ public class PageAdapter extends FragmentPagerAdapter {
     @Nullable
     @Override
     public CharSequence getPageTitle(int position) {
-        switch (position){
+        switch (position) {
             case 0:
                 return "Most Popular";
             case 1:
                 return "Top Stories";
             case 2:
                 return "Technology";
+            default:
+                return null;
         }
     }
 }
