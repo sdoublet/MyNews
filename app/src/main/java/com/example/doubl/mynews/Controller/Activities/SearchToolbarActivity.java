780c0685caca75a460b78abc91a8ecf8495e95ea
sdoublet <doubletsebastien@sfr.fr>
2019-03-12 11:39:04 +0100

    set onclickButton in searchToolbarActivity

--- app/src/main/java/com/example/doubl/mynews/Controller/Activities/SearchToolbarActivity.java
+++ app/src/main/java/com/example/doubl/mynews/Controller/Activities/SearchToolbarActivity.java
@@ -1,17 +1,31 @@
 package com.example.doubl.mynews.Controller.Activities;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.doubl.mynews.R;
 
-public class SearchToolbarActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SearchToolbarActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.search_articles_button)Button searchArticleButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_toolbar);
+        ButterKnife.bind(this);
         this.configureToolbar();
+        searchArticleButton.setOnClickListener(this);
+
     }
 
 
@@ -22,5 +36,16 @@ public class SearchToolbarActivity extends AppCompatActivity {
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
 
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        onButtonClicked();
+    }
+
+    private void onButtonClicked(){
+        Intent intent = new Intent(this, SearchActivity.class);
+        startActivity(intent);
     }
 }
