fd35d8de04fe6100eb4478c8c56ca3b59ec7c9c2
sdoublet <doubletsebastien@sfr.fr>
2019-04-15 18:42:56 +0200

    add two new instrumented tests

--- app/src/androidTest/java/com/example/doubl/mynews/MyNews/Utils/StreamsTest.java
+++ app/src/androidTest/java/com/example/doubl/mynews/MyNews/Utils/StreamsTest.java
@@ -2,6 +2,8 @@ package com.example.doubl.mynews.MyNews.Utils;
 
 
 import com.example.doubl.mynews.MyNews.Models.MostPopular;
+import com.example.doubl.mynews.MyNews.Models.SearchApi;
+import com.example.doubl.mynews.MyNews.Models.TopStories;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -29,6 +31,35 @@ public class StreamsTest {
         MostPopular mostPopular = mostPopularTestObserver.values().get(0);
         Assert.assertTrue(mostPopular.getResults().size() > 0);
 
+    }
+ @Test
+    public void topStories() {
+        Observable<TopStories> observableTopStories = NewYorkTimesStream.streamFetchTopStories("sports");
+
+        TestObserver<TopStories> topStoriesTestObserver = new TestObserver<>();
+
+        observableTopStories.subscribeWith(topStoriesTestObserver)
+                .assertNoErrors()
+                .assertNoTimeout()
+                .awaitTerminalEvent();
+
+        TopStories topStories = topStoriesTestObserver.values().get(0);
+        Assert.assertTrue(topStories.getResults().size() > 0);
+
+    }
+@Test
+    public void searchArticle() {
+        Observable<SearchApi> observableSearchApi = NewYorkTimesStream.streamFetchSearchArticle(null, "20190415",0,"arts",null, "newest", ApiKey.API_KEY );
+
+        TestObserver<SearchApi> searchArticleTestObserver = new TestObserver<>();
+
+        observableSearchApi.subscribeWith(searchArticleTestObserver)
+                .assertNoErrors()
+                .assertNoTimeout()
+                .awaitTerminalEvent();
+
+        SearchApi searchApi = searchArticleTestObserver.values().get(0);
+        Assert.assertTrue(searchApi.getResponse().getDocs().size() > 0);
 
     }
 
--- app/src/main/java/com/example/doubl/mynews/MyNews/Activities/AboutActivity.java
+++ app/src/main/java/com/example/doubl/mynews/MyNews/Activities/AboutActivity.java
@@ -1,17 +1,28 @@
 package com.example.doubl.mynews.MyNews.Activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.doubl.mynews.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class AboutActivity extends AppCompatActivity {
 
+    @BindView(R.id.link_NYT)
+    ImageView linkLogoNYT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        ButterKnife.bind(this);
         this.configureToolbar();
+        clickOnLogoNYT();
     }
 
     private void configureToolbar() {
@@ -20,5 +31,16 @@ public class AboutActivity extends AppCompatActivity {
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         assert actionBar != null;
         actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void clickOnLogoNYT(){
+        linkLogoNYT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://developer.nytimes.com.") );
+
+                startActivity(intent);
+            }
+        });
     }
 }
